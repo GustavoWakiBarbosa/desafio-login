fe2b9881ca23d2ae07b06d721db1b5fcaeba2d5a
waki2k <wakigustavo@gmail.com>
2021-11-26 20:48:05 -0300

    login versão 3

--- login.js
+++ login.js
@@ -1,15 +1,15 @@
-function verificarLogin(){
+function verificarLogin() {
     var emailCerto = document.getElementById("email").value;
     var senhaCerto = document.getElementById("senha").value;
 
-    if(emailCerto == "macacoflavio123@gmail.com"){
-        if(senhaCerto == "seacha123"){
+    if (emailCerto == "macacoflavio123@gmail.com") {
+        if (senhaCerto == "seacha123") {
             alert("Usuário logado com sucesso!")
-        } else{
+        } else {
             alert("Algo deu errado, verefique o email e a senha")
         }
-    
-    }else{
+
+    } else {
         alert("Algo deu errado, verefique o email e a senha")
     }
 }
