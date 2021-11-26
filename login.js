82ea6cd7a79d5c54da13e764f4cba763c02e6112
waki2k <wakigustavo@gmail.com>
2021-11-25 23:44:56 -0300

    login versão 2

--- login.js
+++ login.js
@@ -2,8 +2,8 @@ function verificarLogin(){
     var emailCerto = document.getElementById("email").value;
     var senhaCerto = document.getElementById("senha").value;
 
-    if(emailCerto == "caioba123@gmail.com"){
-        if(senhaCerto == "123"){
+    if(emailCerto == "macacoflavio123@gmail.com"){
+        if(senhaCerto == "seacha123"){
             alert("Usuário logado com sucesso!")
         } else{
             alert("Algo deu errado, verefique o email e a senha")
